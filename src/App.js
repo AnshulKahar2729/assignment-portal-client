e96a3f3255d8a5a98a5725158050bd3f3a1752b9
AnshulKahar2729 <anshulkahar2211@gmail.com>
2024-01-21 23:51:14 +0530

    fake commit

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ function App() {
               <Route path='/courses/coursedetails/*' element={<TeacherCourseDetails/>} />
               <Route path='/courses/coursedetails/files' element={<FilesPage role={role}/>} />
               <Route path='/assignments' element={<Assignments role={role}/>} />
-              {/* <Route path='/assignments/studentdetails/*' element={<StudentCourseDetails/>} /> */}
+              {/* <Route path='/assignments/studentdetails/*' element={<StudentCourseDetails/> } /> */}
               {/* <Route path='/assignments/studentdetails/studentAssignmentInfo' element={<StudentAssignments/>} /> */}
              </Routes>
             }
